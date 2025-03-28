1d7052574286b9fb6042fb243327d2cd8891f72b
Ullr <ullralvarsson@gmail.com>
2025-03-28 20:19:26 +0100

    feat: removed frontend due to conflicts

--- MiProyecto/frontend/src/components/HeaderBar.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <h1>Korso</h1>
-</template>
-
-<script type="ts"></script>
-
-<style scoped>
-
-</style>
--- MiProyecto/frontend/src/views/HomeView.vue
+++
@@ -1,6 +0,0 @@
-<template>
-  <router-link to="">Inicio</router-link> |
-</template>
-<script>
-
-</script>
